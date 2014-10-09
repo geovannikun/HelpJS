a1549025440de11e6a032b2a8e73e6591e0f2d77
Geovanni Pacheco <geovanni.m.pacheco@gmail.com>
2014-10-09 18:46:19 -0300

    new http function

--- helpjs.js
+++ helpjs.js
@@ -57,48 +57,53 @@ var HelpJS = {
             return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
         }
     },
-    Web:{
-        Http: {
-            methods: {
-                OPTIONS: "OPTIONS",
-                GET: "GET",
-                HEAD: "HEAD",
-                POST: "POST",
-                PUT: "PUT",
-                DELETE: "DELETE",
-                TRACE: "TRACE",
-                CONNECT: "CONNECT"
-            },
-            objectToParameters: function(object){
-                var result = [];
-                for(var propertie in object){
-                    result.push(propertie + ":" + object[propertie]);
-                }
-                return "?"+result.join("&");
-            },
-            send: function(url,sucessCallback,errorCallback){
-                url = {
-                    parameters: (HelpJS.web.http.objectToParameters(url.parameters)||""),
-                    url: url.url,
-                    method: url.method||web.http.methods.GET,
-                    async: url.async||true,
-                    user: url.user||"",
-                    password: url.password||"",
-                    data: url.data
-                };
-                var xmlhttp = new XMLHttpRequest();
-                xmlhttp.onreadystatechange=function(){
-                    if (xmlhttp.readyState==4){
-                        if(xmlhttp.status>=200 && xmlhttp.status<300){
-                            sucessCallback(xmlhttp.responseText, xmlhttp.status);
-                        }else {
-                            errorCallback(xmlhttp.responseText, xmlhttp.status);
-                        }
-                    }
-                };
-                xmlhttp.open(url.method, url.url + url.parameters, url.async, url.user, url.password);
-                (url.data)?xmlhttp.send(url.data):xmlhttp.send();
+    Http: {
+        methods: {
+            OPTIONS: "OPTIONS",
+            GET: "GET",
+            HEAD: "HEAD",
+            POST: "POST",
+            PUT: "PUT",
+            DELETE: "DELETE",
+            TRACE: "TRACE",
+            CONNECT: "CONNECT"
+        },
+        parametersToObject: function(url){
+            var result = {};
+            for(var propertie in url.split("?")[1].split("&")){
+                result[propertie.split(":")[0]] = unescape(propertie.split(":")[1]);
             }
+            return result;
+        },
+        objectToParameters: function(object){
+            var result = [];
+            for(var propertie in object){
+                result.push(propertie + ":" + object[propertie]);
+            }
+            return "?"+result.join("&");
+        },
+        send: function(url,sucessCallback,errorCallback){
+            url = {
+                parameters: (HelpJS.web.http.objectToParameters(url.parameters)||""),
+                url: url.url,
+                method: url.method||web.http.methods.GET,
+                async: url.async||true,
+                user: url.user||"",
+                password: url.password||"",
+                data: url.data
+            };
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange=function(){
+                if (xmlhttp.readyState==4){
+                    if(xmlhttp.status>=200 && xmlhttp.status<300){
+                        sucessCallback(xmlhttp.responseText, xmlhttp.status);
+                    }else {
+                        errorCallback(xmlhttp.responseText, xmlhttp.status);
+                    }
+                }
+            };
+            xmlhttp.open(url.method, url.url + url.parameters, url.async, url.user, url.password);
+            (url.data)?xmlhttp.send(url.data):xmlhttp.send();
         }
     }
 };
