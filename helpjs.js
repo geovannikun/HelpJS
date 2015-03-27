d540952f21847cf22c8b80142888ca63f8c2297c
geovanni-pacheco-rca <geovanni.pacheco@rcadigital.com.br>
2015-03-27 13:54:47 -0300

    corrigindo formatação

--- helpjs.js
+++ helpjs.js
@@ -1,63 +1,63 @@
 "use strict";
 var HelpJS = {
-    Array: {
-        select: function (array, action) {
-            var result = [];
-            for (var i = 0, length = array.length; i < length; i++) {
-                result.push(action(array[i]));
-            }
-            return result;
-        },
-        where: function (array, action) {
-            var result = [];
-            for (var i = 0, length = array.length; i < length; i++) {
-                if(action(array[i])){
-                    result.push(array[i]);
-                }
-            }
-            return result;
-        },
-        sum: function (array, action) {
-            var sum = 0;
-            for (var i = 0, length = array.length; i < length; i++) {
-                sum += action(array[i]);
-            }
-            return sum;
-        },
-        clear: function (array) {
-            while (array.length) {
-                array.pop();
-            }
-        },
-        limit: function (array, limit) {
-            var result = [];
-            for (var i = 0, length = array.length; i < limit && i < length; i++) {
-                result.push(array[i]);
-            }
-            return result;
-        },
-        keys: function (array) {
-            result = [];
-            for (var key in Object.keys(array)) {
-                if (arr.hasOwnProperty(key)) {
-                    result.push(array[i]);
-                }
-            }
-            return result;
-        }
-    },
-    Number: {
-        pad: function (str, max) {
-            str = str.toString();
-            return str.length < max ? HelpJS.Number.pad("0" + str, max) : str;
-        }
-    },
-    String: {
-        replaceAll: function (str, find, replace) {
-            return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
-        }
-    },
-    Http: {
+	Array: {
+		select: function (array, action) {
+			var result = [];
+			for (var i = 0, length = array.length; i < length; i++) {
+				result.push(action(array[i]));
+			}
+			return result;
+		},
+		where: function (array, action) {
+			var result = [];
+			for (var i = 0, length = array.length; i < length; i++) {
+				if(action(array[i])){
+					result.push(array[i]);
+				}
+			}
+			return result;
+		},
+		sum: function (array, action) {
+			var sum = 0;
+			for (var i = 0, length = array.length; i < length; i++) {
+				sum += action(array[i]);
+			}
+			return sum;
+		},
+		clear: function (array) {
+			while (array.length) {
+				array.pop();
+			}
+		},
+		limit: function (array, limit) {
+			var result = [];
+			for (var i = 0, length = array.length; i < limit && i < length; i++) {
+				result.push(array[i]);
+			}
+			return result;
+		},
+		keys: function (array) {
+			result = [];
+			for (var key in Object.keys(array)) {
+				if (arr.hasOwnProperty(key)) {
+					result.push(array[i]);
+				}
+			}
+			return result;
+		}
+	},
+	Number: {
+		pad: function (str, max) {
+			str = str.toString();
+			return str.length < max ? HelpJS.Number.pad("0" + str, max) : str;
+		}
+	},
+	String: {
+		replaceAll: function (str, find, replace) {
+			return str.replace(new RegExp(find.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'), 'g'), replace);
+		}
+	},
+	Http: {
 		Cookie: {
 			set: function(key,value,life){
 				var d = new Date();
@@ -90,56 +90,56 @@ var HelpJS = {
 				return false;
 			},
 		},
-        methods: {
-            OPTIONS: "OPTIONS",
-            GET: "GET",
-            HEAD: "HEAD",
-            POST: "POST",
-            PUT: "PUT",
-            DELETE: "DELETE",
-            TRACE: "TRACE",
-            CONNECT: "CONNECT"
-        },
-        parametersToObject: function(url){
-            var result = {};
-            for(var propertie in url.split("?")[1].split("&")){
-                result[propertie.split(":")[0]] = unescape(propertie.split(":")[1]);
-            }
-            return result;
-        },
-        objectToParameters: function(object){
-            var result = [];
-            for(var propertie in object){
-                result.push(propertie + "=" + object[propertie]);
-            }
-            return "?"+result.join("&");
-        },
-        send: function(request,sucessCallback,errorCallback){
-            request = {
-                parameters: (request.parameters?HelpJS.Http.objectToParameters(request.parameters):""),
-                url: request.url,
-                method: request.method||"GET",
-                async: request.async||true,
-                user: request.user||"",
-                password: request.password||"",
-                data: request.data,
-                headers: request.headers||{}
-            };
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange=function(){
-                if (xmlhttp.readyState==4){
-                    if(xmlhttp.status>=200 && xmlhttp.status<300){
-                        sucessCallback(xmlhttp.responseText, xmlhttp.status);
-                    }else {
-                        errorCallback(xmlhttp.responseText, xmlhttp.status);
-                    }
-                }
-            };
-            xmlhttp.open(url.method, url.url + url.parameters, url.async, url.user, url.password);
-            for(var propertie in url.headers){
-            	xmlhttp.setRequestHeader(propertie, object[propertie]);
-            }
-            (url.data)?xmlhttp.send(url.data):xmlhttp.send();
-        }
-    }
+		methods: {
+			OPTIONS: "OPTIONS",
+			GET: "GET",
+			HEAD: "HEAD",
+			POST: "POST",
+			PUT: "PUT",
+			DELETE: "DELETE",
+			TRACE: "TRACE",
+			CONNECT: "CONNECT"
+		},
+		parametersToObject: function(url){
+			var result = {};
+			for(var propertie in url.split("?")[1].split("&")){
+				result[propertie.split(":")[0]] = unescape(propertie.split(":")[1]);
+			}
+			return result;
+		},
+		objectToParameters: function(object){
+			var result = [];
+			for(var propertie in object){
+				result.push(propertie + "=" + object[propertie]);
+			}
+			return "?"+result.join("&");
+		},
+		send: function(request,sucessCallback,errorCallback){
+			request = {
+				parameters: (request.parameters?HelpJS.Http.objectToParameters(request.parameters):""),
+				url: request.url,
+				method: request.method||"GET",
+				async: request.async||true,
+				user: request.user||"",
+				password: request.password||"",
+				data: request.data,
+				headers: request.headers||{}
+			};
+			var xmlhttp = new XMLHttpRequest();
+			xmlhttp.onreadystatechange=function(){
+				if (xmlhttp.readyState==4){
+					if(xmlhttp.status>=200 && xmlhttp.status<300){
+						sucessCallback(xmlhttp.responseText, xmlhttp.status);
+					}else {
+						errorCallback(xmlhttp.responseText, xmlhttp.status);
+					}
+				}
+			};
+			xmlhttp.open(url.method, url.url + url.parameters, url.async, url.user, url.password);
+			for(var propertie in url.headers){
+				xmlhttp.setRequestHeader(propertie, object[propertie]);
+			}
+			(url.data)?xmlhttp.send(url.data):xmlhttp.send();
+		}
+	}
 };
